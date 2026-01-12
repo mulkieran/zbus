73ba1022431044045e9dc9afe1f5ffd5490477b9
mulhern <amulhern@redhat.com>
2026-01-12 14:14:01 -0500

    Remove DispatchResult::new_async to test deadness
    It is possible that this method is not used at all, as compilation
    proceeds regardless.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- zbus/src/object_server/interface/dispatch_result.rs
+++ zbus/src/object_server/interface/dispatch_result.rs
@@ -1,10 +1,6 @@
 use std::{future::Future, pin::Pin};
 
-use zbus::message::Flags;
-use zvariant::DynamicType;
-
-use crate::{Connection, Result, message::Message};
-use tracing::trace;
+use crate::Result;
 
 /// A helper type returned by [`Interface`](`crate::object_server::Interface`) callbacks.
 pub enum DispatchResult<'a> {
@@ -18,29 +14,4 @@ pub enum DispatchResult<'a> {
 
     /// The method was found and will be completed by running this Future.
     Async(Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>),
-}
-
-impl<'a> DispatchResult<'a> {
-    /// Helper for creating the Async variant.
-    pub fn new_async<F, T, E>(conn: &'a Connection, msg: &'a Message, f: F) -> Self
-    where
-        F: Future<Output = ::std::result::Result<T, E>> + Send + 'a,
-        T: serde::Serialize + DynamicType + Send + Sync,
-        E: zbus::DBusError + Send,
-    {
-        DispatchResult::Async(Box::pin(async move {
-            let hdr = msg.header();
-            let ret = f.await;
-            if !hdr.primary().flags().contains(Flags::NoReplyExpected) {
-                match ret {
-                    Ok(r) => conn.reply(&hdr, &r).await,
-                    Err(e) => conn.reply_dbus_error(&hdr, e).await,
-                }
-                .map(|_seq| ())
-            } else {
-                trace!("No reply expected for {:?} by the caller.", msg);
-                Ok(())
-            }
-        }))
-    }
 }
